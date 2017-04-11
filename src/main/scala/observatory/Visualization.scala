7e1981bbdf7df603f11ece5149a209a1daa7d677
akudryavets <a.kudryavets@cleverdata.ru>
2017-04-11 20:51:59 +0300

    locateTemperatures added

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,15 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.Image
+
+import scala.collection.parallel.immutable.ParVector
+import scala.math._
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
+  val DEFAULT_POWER_PARAMETER = 2D
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,8 +17,34 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+    val tempVec = temperatures.toVector.par
+
+    tempVec.find{ case (loc, _) => loc == location }.map{ case (_, temp) => temp}
+      .getOrElse(predictTemperatureImpl(tempVec, location, DEFAULT_POWER_PARAMETER))
   }
+
+  def predictTemperatureImpl(temperatures: ParVector[(Location, Double)],
+                             location: Location,
+                             p: Double): Double = {
+    val (sumWeightedTemps, sumWeghts) = temperatures
+      .map{ case (loc, temp) => (temp, 1 / pow(distance(loc2Rad(loc), loc2Rad(location)), p))}
+      .aggregate(0D, 0D) (
+        (acc, tempWeigtPair) => (acc._1 + tempWeigtPair._1 * tempWeigtPair._2, acc._2 + tempWeigtPair._2),
+        (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+      )
+
+    sumWeightedTemps / sumWeghts
+  }
+
+  def distance(location1: Location, location2: Location): Double =
+    acos(
+      sin(location1.lat) * sin(location2.lat) + cos(location1.lat) * cos(location2.lat) * cos(
+        abs(location1.lon - location2.lon)
+      )
+    )
+
+  def loc2Rad(loc: Location): Location = Location(toRadians(loc.lat), toRadians(loc.lon))
+
 
   /**
     * @param points Pairs containing a value and its associated color
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -2,12 +2,64 @@ package observatory
 
 
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.collection.parallel.immutable.ParVector
 
 @RunWith(classOf[JUnitRunner])
-class VisualizationTest extends FunSuite with Checkers {
+class VisualizationTest extends FunSuite with Checkers with Matchers {
+  import Visualization._
+
+  test("basic distance test") {
+    assert(distance(loc2Rad(Location(0, 90)), loc2Rad(Location(180, 90))) === math.Pi +- .0001)
+    assert(distance(loc2Rad(Location(90, 0)), loc2Rad(Location(-90, 0))) === math.Pi +- .0001)
+    assert(distance(loc2Rad(Location(180, 90)), loc2Rad(Location(180, 90))) === 0D +- .0001)
+    assert(distance(loc2Rad(Location(180, 90)), loc2Rad(Location(0, -90))) === 0D +- .0001)
+  }
+
+  test("basic predictTemperatureImpl test") {
+    val temperatures: ParVector[(Location, Double)] = ParVector(
+          (Location(1D, 1D), 1D),
+          (Location(1D, -1D), 2D),
+          (Location(-1D, -1D), 3D),
+          (Location(-10D, 10D), 60D)
+        )
+
+    val predicted: Double = predictTemperatureImpl(temperatures, Location(0D, 0D), DEFAULT_POWER_PARAMETER)
+
+    assert(predicted === 2.194 +- 0.001)
+  }
+
+  test("predictTemperatureImpl neighbourhood") {
+    val loc1 = Location(0, 90)
+    val temp1 = 50
+    val loc2 = Location(180, 90)
+    val temp2 = 10
+
+    val temperatures: ParVector[(Location, Double)] = ParVector(
+      (loc1, temp1),
+      (loc2, temp2)
+    )
+
+    val genLoc = Gen.resultOf(Location)
+
+    forAll(genLoc) { loc: Location =>
+      val predicted: Double = predictTemperatureImpl(temperatures, loc, DEFAULT_POWER_PARAMETER)
+      if (distance(loc2Rad(loc), loc2Rad(loc1)) > distance(loc2Rad(loc), loc2Rad(loc2))) {
+        temp1 - predicted > temp2 - predicted
+      } else {
+        temp1 - predicted <= temp2 - predicted
+      }
+    }
+  }
+
+//  test("basic interpolation test") {
+//
+//  }
 
 
 }
